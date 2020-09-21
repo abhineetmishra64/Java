cbd8b30feceeb4ef80c28df002f8ad4060865ccd
Abhineet Mishra <42692851+abhineetmishra64@users.noreply.github.com>
2020-09-21 11:57:53 +0530

    Uses of different method in one class

---
+++ day.java
@@ -0,0 +1,9 @@
+class Day{
+    public static void MyMethod(){
+        System.out.println("Hello there, this is MyMethod");
+    }
+    public static void main(String []args){
+        Day obj = new Day();
+        obj.MyMethod();
+    }
+}
