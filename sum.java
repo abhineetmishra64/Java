46ae3d43c5bc246aeca6067b18a275a3b67d75c0
Abhineet Mishra <42692851+abhineetmishra64@users.noreply.github.com>
2020-09-25 12:56:31 +0530

    Sum of two number taken as command line input

---
+++ sum.java
@@ -0,0 +1,8 @@
+class Sum{
+	public static void main(String []args){
+		int num1=Integer.parseInt(args[0]);
+		int num2=Integer.parseInt(args[1]);
+		int c=num1+num2;
+		System.out.println("Sum ="+c);
+	}
+}
