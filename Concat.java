58601d03bdd2c20c07f9db057da1631879d6f293
Abhineet Mishra <42692851+abhineetmishra64@users.noreply.github.com>
2020-12-09 14:28:27 +0530

    Merge two arrays in java

--- Concat.java
+++ Concat.java
@@ -1,8 +1,28 @@
 import java.util.Arrays;
 import java.util.Scanner;
+
+class concat1{
+    public void arrConcat(int[] array1,int[] array2, int length){
+        int[] result = new int[length];
+        int pos = 0;
+        for (int i=0;i<array1.length;i++) {
+            result[pos] = array1[i];
+            pos++;
+        }
+
+        for (int i=0;i<array2.length;i++) {
+            result[pos] = array2[i];
+            pos++;
+        }
+        System.out.println("Concated array");
+        System.out.println(Arrays.toString(result));
+    }
+}
+
 public class Concat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        concat1 ca = new concat1();
         System.out.print("Enter the size of array1");
         int n = sc.nextInt();
         int[] array1 = new int[n];
@@ -17,21 +37,9 @@ public class Concat {
         for(int i=0;i<l;i++){
             array2[i] = sc.nextInt();
         }
+        sc.close();
 
         int length = array1.length + array2.length;
-
-        int[] result = new int[length];
-        int pos = 0;
-        for (int i=0;i<array1.length;i++) {
-            result[pos] = array1[i];
-            pos++;
-        }
-
-        for (int i=0;i<array2.length;i++) {
-            result[pos] = array2[i];
-            pos++;
-        }
-        System.out.println("Concated array");
-        System.out.println(Arrays.toString(result));
+        ca.arrConcat(array1, array2, length);
     }
 }
