05a5a7a6b0fc2459463dbe3b13c12db68488b6d9
Abhineet Mishra <42692851+abhineetmishra64@users.noreply.github.com>
2020-12-09 14:29:16 +0530

    Sort array in ascending and descending order

--- array.java
+++ array.java
@@ -1,18 +1,8 @@
 import java.util.Scanner;
 import java.util.Arrays;
-public class array{
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        array ar = new array();
-        System.out.print("Enter the size of array");
-        int n = scan.nextInt();
 
-        int a[] = new int[n];
-        System.out.println("Enter the elements of array");
-        for(int i=0; i<n;i++){
-            a[i] = scan.nextInt();
-        }
-        scan.close();
+class program{
+    public void asc(int n, int [] a){
         for(int j=0;j<n;j++){
             for(int i=j+1;i<n;i++){
                 if(a[j]>a[i]){
@@ -22,19 +12,42 @@ public class array{
                 }
             }
         }
+    }
+
+    public int[] dsc(int n, int [] a){
         int[] b = new int[n]; 
         int j = n; 
         for (int i = 0; i < n; i++) { 
             b[j - 1] = a[i]; 
             j = j - 1; 
         }
-        ar.see(n,a,b);
+        return b;
     }
 
-    public void see(int n,int a[],int b[]){
-        System.out.println("Array in asscending order");
+    public void see(int n,int a[]){
         System.out.println(Arrays.toString(a));
-        System.out.println("Array in descending order");
-        System.out.println(Arrays.toString(b));
+    }
+}
+
+public class array{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the size of array");
+        int n = scan.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter the elements of array");
+        for(int i=0; i<n;i++){
+            a[i] = scan.nextInt();
+        }
+        program pr = new program();
+        System.out.println("Original array");
+        pr.see(n,a);
+        pr.asc(n,a);
+        System.out.println("Array after sorting in ascending order");
+        pr.see(n,a);
+        int[] x = pr.dsc(n,a);
+        System.out.println("Array after sorting in descending order");
+        pr.see(n,x);
+        scan.close(); 
     }
 }
