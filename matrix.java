133dc117d0fd4f358cdf1216042db2e755dc67d3
Abhineet Mishra <42692851+abhineetmishra64@users.noreply.github.com>
2020-12-09 14:30:29 +0530

    Perform different operation in matrix

--- matrix.java
+++ matrix.java
@@ -1,33 +1,6 @@
 import java.util.Scanner;
-public class matrix {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        matrix mt = new matrix();
-        System.out.println("Enter no. of rows");
-        int r = sc.nextInt();
-        System.out.println("Enter no. of coloumn");
-        int c = sc.nextInt();
-        int[][] matrix1 = new int[r][c];
-        int[][] matrix2 = new int[r][c];
-        System.out.println("Enter the element in matrix 1: ");
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                matrix1[i][j] = sc.nextInt();
-            }
-        }
-        System.out.println("Enter the elements in matrix 2: ");
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                matrix2[i][j] = sc.nextInt();
-            }
-        }
-        sc.close();
-        int[][] sum = mt.sum(r, c, matrix1, matrix2);
-        int[][] diff = mt.diff(r,c,matrix1,matrix2);
-        int[][] multi = mt.multi(r,c,matrix1,matrix2);
-        mt.display(r, c, sum, diff, multi);     
-    }
 
+class matrix1{
     public int[][] sum(int r, int c, int matrix1[][], int matrix2[][]){
         int[][] sum = new int[r][c];
         for(int i=0;i<r;i++){
@@ -78,5 +51,35 @@ public class matrix {
                 System.out.println(multi[i][j]);
             }
         }
+    }
+}
+
+public class matrix {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        matrix1 mt = new matrix1();
+        System.out.println("Enter no. of rows");
+        int r = sc.nextInt();
+        System.out.println("Enter no. of coloumn");
+        int c = sc.nextInt();
+        int[][] matrix1 = new int[r][c];
+        int[][] matrix2 = new int[r][c];
+        System.out.println("Enter the element in matrix 1: ");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Enter the elements in matrix 2: ");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+        int[][] sum = mt.sum(r, c, matrix1, matrix2);
+        int[][] diff = mt.diff(r,c,matrix1,matrix2);
+        int[][] multi = mt.multi(r,c,matrix1,matrix2);
+        mt.display(r, c, sum, diff, multi);     
     }
 }
